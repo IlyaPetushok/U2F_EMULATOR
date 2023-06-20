2502e9284bdf36b40768720848917bf0e727dff6
Petushok Ilya <a3310010752@mail.ru>
2023-06-20 23:43:49 +0300

    message

--- src/main/java/u2f/emulator/KeyboardListener.java
+++ src/main/java/u2f/emulator/KeyboardListener.java
@@ -1,27 +1,36 @@
 package u2f.emulator;
 
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class KeyboardListener {
     Scanner scanner = new Scanner(System.in);
 
     public boolean keyPressed = true;
 
-    public KeyboardListener() {
+    public KeyboardListener()  {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Time is up!");
+                System.exit(0);
+            }
+        }, 30000);
+
         while (!keyPressed) {
             System.out.println("Click Enter to confirm your presence");
-            if (scanner.hasNextLine()) {
-                String input = scanner.nextLine();
-                if (input.equals("") || scanner.hasNext()) {
-                    keyPressed();
-                }
+            String input = scanner.nextLine();
+            if (input.equals("") || scanner.hasNext()) {
+                keyPressed();
             }
         }
-
-
+        timer.cancel();
     }
 
-    private void keyPressed() {
+    public void keyPressed() {
         keyPressed = true;
     }
+
 }
